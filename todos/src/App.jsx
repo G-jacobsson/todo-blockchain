f3fee80c77d7043336357206575a2bf6dc06ee0c
G-O Jacobsson <geo.jacobsson@gmail.com>
2024-03-18 13:39:05 +0100

    :sparkles: Import ethers and define provider

--- todos/src/App.jsx
+++ todos/src/App.jsx
@@ -1,4 +1,12 @@
 import './App.css';
+import { ethers } from 'ethers';
+
+if (window.ethereum) {
+  window.provider = new ethers.BrowserProvider(window.ethereum);
+  console.log(window.provider);
+} else {
+  console.error('Metamask not found');
+}
 
 function App() {
   return <></>;
