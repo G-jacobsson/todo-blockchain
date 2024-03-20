14a4a14ca5d00285a6ab24cb3138dbe39285fe0c
G-O Jacobsson <geo.jacobsson@gmail.com>
2024-03-20 21:34:05 +0100

    :zap: Improve blockchainService and App.jsx with read and write states and logic

--- todos/src/App.jsx
+++ todos/src/App.jsx
@@ -2,49 +2,67 @@ import './App.css';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import { useEffect, useState } from 'react';
-import {
-  createTodo,
-  getTodos,
-  removeTodo,
-  toggleTodo,
+import blockchainService, {
+  getReadContract,
+  getWriteContract,
 } from './services/blockchainService';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [error, setError] = useState('');
+  const [readContract, setReadContract] = useState();
+  const [writeContract, setWriteContract] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setReadContract(getReadContract());
+        setWriteContract(getWriteContract());
+      } catch (error) {
+        setError(`Failed to get contract: ${error.message}`);
+      }
+    })();
+  }, []);
 
   useEffect(() => {
     const todosInit = async () => {
       try {
-        const updatedTodos = await getTodos();
-        console.log('Todos fetched from contract:', updatedTodos);
+        const updatedTodos = await blockchainService.getTodos();
         setTodos(updatedTodos);
       } catch (error) {
         setError(error.message);
       }
     };
-    todosInit();
-  }, []);
+    if (readContract) {
+      todosInit();
+    }
+  }, [readContract]);
 
   const addNewTodo = async (text) => {
-    await createTodo(text);
-    const updatedTodos = await getTodos();
-    setTodos(updatedTodos);
-  };
+    if (!text.trim()) {
+      alert('Please enter a valid todo.');
+      return;
+    }
 
-  const deleteTodo = async (id) => {
     try {
-      await removeTodo(id);
-      const updatedTodos = await getTodos();
+      await blockchainService.createTodo(text);
+      const updatedTodos = await blockchainService.getTodos(readContract);
       setTodos(updatedTodos);
     } catch (error) {
-      console.error('Failed to remove todo:', error);
+      console.error('Failed to add todo:', error);
+      setError(`Failed to add todo: ${error.message}`);
     }
   };
 
+  const deleteTodo = async (id) => {
+    await blockchainService.removeTodo(id);
+    const updatedTodos = await blockchainService.getTodos(readContract);
+    setTodos(updatedTodos);
+  };
+
   const toggleTodos = async (id) => {
-    await toggleTodo(id);
-    const updatedTodos = await getTodos();
+    await blockchainService.toggleTodo(id);
+    const updatedTodos = await blockchainService.getTodos(readContract);
     setTodos(updatedTodos);
   };
 
--- todos/src/services/blockchainService.js
+++ todos/src/services/blockchainService.js
@@ -11,59 +11,64 @@ const getProvider = () => {
   }
 };
 
+export const getReadContract = () => {
+  const provider = getProvider();
+  return new ethers.Contract(address, ABI, provider);
+};
+
+export const getWriteContract = () => {
+  const provider = getProvider();
+  const signer = provider.getSigner();
+  return new ethers.Contract(address, ABI, signer);
+};
+
 const requestAccount = async () => {
   await window.ethereum.request({ method: 'eth_requestAccounts' });
 };
 
-export const createTodo = async (text) => {
-  const provider = getProvider();
+const blockchainService = {
+  createTodo: async (text) => {
+    await requestAccount();
+    const contract = getWriteContract();
+    console.log(`Creating todo: ${text}`);
+    const transaction = await contract.createTodo(text);
+    await transaction.wait();
+    console.log(`Todo created: ${text}`);
+  },
 
-  await requestAccount();
-  const signer = provider.getSigner();
-  const contract = new ethers.Contract(address, ABI, signer);
+  removeTodo: async (id) => {
+    await requestAccount();
+    const contract = getWriteContract();
+    console.log(`Removing todo: ${id}`);
+    const transaction = await contract.removeTodo(id);
+    await transaction.wait();
+    console.log(`Todo removed: ${id}`);
+  },
 
-  const transaction = await contract.createTodo(text);
-  await transaction.wait();
-};
+  toggleTodo: async (id) => {
+    await requestAccount();
+    const contract = getWriteContract();
+    const transaction = await contract.toggleTodo(id);
+    await transaction.wait();
+  },
 
-export const removeTodo = async (id) => {
-  const provider = getProvider();
+  getTodos: async () => {
+    const contract = getReadContract();
+    const todosCount = await contract.todoCount();
+    const todos = [];
 
-  await requestAccount();
-  const signer = provider.getSigner();
-  const contract = new ethers.Contract(address, ABI, signer);
-
-  const transaction = await contract.removeTodo(id);
-  await transaction.wait();
-};
-
-export const toggleTodo = async (id) => {
-  const provider = getProvider();
-
-  await requestAccount();
-  const signer = provider.getSigner();
-  const contract = new ethers.Contract(address, ABI, signer);
-
-  const transaction = await contract.toggleTodo(id);
-  await transaction.wait();
-};
-
-export const getTodos = async () => {
-  const provider = getProvider();
-  const contract = new ethers.Contract(address, ABI, provider);
-
-  const todosCount = await contract.todoCount();
-  const todos = [];
-
-  for (let i = 1; i <= todosCount.toNumber(); i++) {
-    const todo = await contract.todos(i);
-    if (todo.id.toNumber() !== 0) {
-      todos.push({
-        id: todo.id.toNumber(),
-        text: todo.text,
-        completed: todo.completed,
-      });
+    for (let i = 1; i <= todosCount.toNumber(); i++) {
+      const todo = await contract.todos(i);
+      if (todo.id.toNumber() !== 0) {
+        todos.push({
+          id: todo.id.toNumber(),
+          text: todo.text,
+          completed: todo.completed,
+        });
+      }
     }
-  }
-  return todos;
+    return todos;
+  },
 };
+
+export default blockchainService;
